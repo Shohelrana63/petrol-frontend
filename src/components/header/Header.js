05c7f33ccf4e8e2a17b9aae103fcaf640886181f
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2022-11-05 00:53:28 +0600

    responsive in WIP

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,22 +6,22 @@ import {BsTwitter, BsInstagram} from 'react-icons/bs';
 const Header = () => {
   return (
     <div className='bg-headerbg py-3.5 border-b-2 border-yellowborder'>
-        <div className='max-w-container mx-auto'>
-        <div className='flex'>
-            <div className='w-2/4 flex'>
+        <div className='max-w-container mx-auto px-2.5 md:px-0'>
+        <div className='sml:flex'>
+            <div className='sml:w-9/12 sml:flex'>
                 <p className='font-pops font-normal text-white relative pl-6
                 after:absolute after:top-0 after:right-[-28px] after:w-px after:h-full after:content-[" "]
                 after:bg-yellowborder'>
                 <AiOutlineMail className='absolute left-0 top-1 text-lg'/>
                 mail@yourcompany.com
                 </p>
-                <p className='font-pops font-normal text-white relative pl-6 ml-14'>
-                <FaPhoneAlt className='absolute left-0 top-1 text-lg'/>
-                mail@yourcompany.com
+                <p className='font-pops font-normal text-white relative pl-6 sml:ml-14 my-2.5 sml:my-0'>
+                <FaPhoneAlt className='absolute left-0 top-0 text-lg'/>
+                +896 120 5889 (Toll free)
                 </p>
             </div>
-            <div className='w-2/4 flex justify-end'>
-              <FaFacebookF className='text-white text-lg ml-5'/>
+            <div className='sml:w-3/12 flex sml:justify-end'>
+              <FaFacebookF className='text-white text-lg sml:ml-5'/>
               <BsTwitter className='text-white text-lg ml-5'/>
               <FaLinkedinIn className='text-white text-lg ml-5'/>
               <BsInstagram className='text-white text-lg ml-5'/>
